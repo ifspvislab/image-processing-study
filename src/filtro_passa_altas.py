a908721a6df330dd242a237826292439dbc15baa
Jonas Sanjinez Lopes <batmancarecazz@gmail.com>
2023-11-06 19:56:14 -0300

    add matplotlib visualization

--- src/filtro_passa_altas.py
+++ src/filtro_passa_altas.py
@@ -1,12 +1,30 @@
 from PIL import Image, ImageFilter
+import matplotlib.pyplot as plt
 
-imagem = Image.open("assets/foto1.jpg").convert("L")
+def filtro_passa_altas(image):
+    # Cria um filtro passa-alta simples
+    mascara = ImageFilter.Kernel((3, 3), [-1, -1, -1, -1, 8, -1, -1, -1, -1], scale=1)
 
-# Cria a mascara 3x3 do filtro passa-altas
-mascara = ImageFilter.Kernel((3, 3), [-1, -1, -1, -1, 8, -1, -1, -1, -1], scale=9)
+    imagem_filtrada = image.filter(mascara)
 
-# Aplica a convolução com a mascara 3x3
-imagem_passa_altas = imagem.filter(mascara)
+    return imagem_filtrada
 
-# Mostra a imagem gerada
-imagem_passa_altas.show()
+if __name__ == "__main__":
+    imagem = Image.open("assets/foto1.jpg").convert("L")
+
+    # Aplica o filtro passa-alta
+    imagem_passa_altas = filtro_passa_altas(imagem)
+
+    # Exibe a imagem original e a imagem filtrada
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.imshow(imagem, cmap="gray")
+    plt.title("Imagem original (cinza)")
+    plt.axis("off")
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(imagem_passa_altas, cmap="gray")
+    plt.title("Filtro passa-altas")
+    plt.axis("off")
+
+    plt.show()
