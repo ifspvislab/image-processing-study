d18c259260f7f96ae5b4d03e65a7cc812e01e814
Jonas Sanjinez Lopes <batmancarecazz@gmail.com>
2023-11-06 20:17:49 -0300

    add matplotlib visualization

--- src/filtro_mediana.py
+++ src/filtro_mediana.py
@@ -1,12 +1,30 @@
 from PIL import Image, ImageFilter
+import matplotlib.pyplot as plt
 
-imagem = Image.open("assets/foto2.jpg").convert("L")
+def filtro_mediana(image):
+    # Cria o tamanho do filtro da mediana
+    raio = 3
 
-# Cria o tamanho do filtro da mediana
-raio = 3
+    imagem_filtrada = image.filter(ImageFilter.MedianFilter(size=raio))
 
-# Aplica o filtro na imagem
-imagem_filtro_mediana = imagem.filter(ImageFilter.MedianFilter(size=raio))
+    return imagem_filtrada
 
-# Mostra a imagem gerada
-imagem_filtro_mediana.show()
+if __name__ == "__main__":
+    imagem = Image.open("assets/foto2.jpg").convert("L")
+   
+    # Aplica o filtro da media
+    imagem_mediana = filtro_mediana(imagem)
+
+    # Exibe a imagem original e a imagem filtrada
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.imshow(imagem, cmap="gray")
+    plt.title("Imagem original (cinza)")
+    plt.axis("off")
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(imagem_mediana, cmap="gray")
+    plt.title("Filtro da mediana")
+    plt.axis("off")
+
+    plt.show()
